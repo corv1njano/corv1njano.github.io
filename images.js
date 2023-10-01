618a1bf0c3a8ac76c28cf8da89f5962867e187b3
corv1njano <corvinjano@gmail.com>
2023-10-01 15:02:48 +0200

    responsive
    handy test 1

--- images.js
+++ images.js
@@ -1,15 +1,17 @@
-function img01() {
+function outfit01() {
     const page = document.getElementById("wrapper");
     page.style.display = "none";
     var img = document.createElement("img");
     img.setAttribute("src", "assets/outfits/01.png");
-    img.setAttribute("id", "image-view");
+    img.setAttribute("id", "image-view-outfit");
     var txt = document.createElement("p");
     var txtNode = document.createTextNode("Click anywhere to close the image.");
     txt.setAttribute("id", "image-text");
-    const body = document.getElementById("body");
-    body.appendChild(img);
+    const div = document.getElementById("outfitDemo");
+    div.setAttribute("id", "outfitDemoActive")
+    div.appendChild(img);
     txt.appendChild(txtNode);
+    const body = document.getElementById("body");
     body.appendChild(txt);
     setTimeout(function(){
         body.setAttribute("onclick", "test()")
@@ -19,8 +21,9 @@ function img01() {
 function test() {
     const page = document.getElementById("wrapper");
     page.style.display = "block";
-    document.getElementById("image-view").remove();
+    document.getElementById("image-view-outfit").remove();
     document.getElementById("image-text").remove();
     const body = document.getElementById("body");
     body.removeAttribute("onclick");
+    document.getElementById("outfitDemoActive").setAttribute("id", "outfitDemo");
 }
