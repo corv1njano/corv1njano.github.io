08d4bdb5eda5a00004f475878f49f2d9b6f6ea3d
corv1njano <corvinjano@gmail.com>
2023-10-01 18:50:16 +0200

    responsive tweaks 2
    image resize tweaks (handy)

--- images.js
+++ images.js
@@ -17,6 +17,44 @@ function outfit01() {
         body.setAttribute("onclick", "test()")
     },500)
 }
+function outfit02() {
+    const page = document.getElementById("wrapper");
+    page.style.display = "none";
+    var img = document.createElement("img");
+    img.setAttribute("src", "assets/outfits/02.png");
+    img.setAttribute("id", "image-view-outfit");
+    var txt = document.createElement("p");
+    var txtNode = document.createTextNode("Click anywhere to close the image.");
+    txt.setAttribute("id", "image-text");
+    const div = document.getElementById("outfitDemo");
+    div.setAttribute("id", "outfitDemoActive")
+    div.appendChild(img);
+    txt.appendChild(txtNode);
+    const body = document.getElementById("body");
+    body.appendChild(txt);
+    setTimeout(function(){
+        body.setAttribute("onclick", "test()")
+    },500)
+}
+function outfit03() {
+    const page = document.getElementById("wrapper");
+    page.style.display = "none";
+    var img = document.createElement("img");
+    img.setAttribute("src", "assets/outfits/03.png");
+    img.setAttribute("id", "image-view-outfit");
+    var txt = document.createElement("p");
+    var txtNode = document.createTextNode("Click anywhere to close the image.");
+    txt.setAttribute("id", "image-text");
+    const div = document.getElementById("outfitDemo");
+    div.setAttribute("id", "outfitDemoActive")
+    div.appendChild(img);
+    txt.appendChild(txtNode);
+    const body = document.getElementById("body");
+    body.appendChild(txt);
+    setTimeout(function(){
+        body.setAttribute("onclick", "test()")
+    },500)
+}
 
 function test() {
     const page = document.getElementById("wrapper");
