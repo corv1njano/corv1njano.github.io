b81743970d88623fe41c435f5064dcfcfe11547e
corv1njano <corvinjano@gmail.com>
2023-10-01 02:28:48 +0200

    update
    new website, gta

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// not my code, credits to: https://stackoverflow.com/questions/37358307/javascript-countdown-timer-to-specific-time-everyday
+
+(function () {
+    var start = new Date;
+    start.setHours(48, 0, 0); //Zeitraum setzen
+
+    function pad(num) {
+        return ("0" + parseInt(num)).substr(-2);
+    }
+
+    function tick() {
+        var now = new Date;
+        if (now > start) { // too late, go to tomorrow
+            start.setDate(start.getDate() + 1);
+        }
+        var remain = ((start - now) / 1000);
+        var hh = pad((remain / 60 / 60) % 60);
+        var mm = pad((remain / 60) % 60);
+        var ss = pad(remain % 60);
+        document.getElementById('time').innerHTML =
+            hh + ":" + mm + ":" + ss;
+        setTimeout(tick, 1000);
+    }
+
+    document.addEventListener('DOMContentLoaded', tick);
+})();
